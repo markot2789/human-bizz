058037ad55823df534ace2a22f9540d5eb5ff5a1
Nikola <nikola.liamg@gmail.com>
2016-03-28 11:44:04 +0200

    initital commit

---
+++ src/main/java/com/humanbizz/web/controllers/IndexController.java
@@ -0,0 +1,24 @@
+package com.humanbizz.web.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.humanbizz.web.services.UserService;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	  @Autowired 
+	  private UserService userService;
+	   
+	  
+	  @RequestMapping(method = RequestMethod.GET)
+	  public String listAll(Model model) {
+	    model.addAttribute("persons", userService.getAllUsers());
+	    return "home";
+	  }
+}
---
+++ src/main/java/com/humanbizz/web/services/UserService.java
@@ -0,0 +1,47 @@
+package com.humanbizz.web.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.humanbizz.web.entities.User;
+
+@Service
+public class UserService {
+
+	  @PersistenceContext
+	  private EntityManager em;
+	  
+	  /**
+	   * Creates 3 fake Users 
+	   * @return List of Users
+	   */
+	  @Transactional
+	  public List<User> getAllUsers(){
+		  
+		  List<User> fakeData = new ArrayList<User>();
+
+		  User fakeUser1 = new User();
+		  fakeUser1.setId(0);
+		  fakeUser1.setName("Adam");
+		  
+		  User fakeUser2 = new User();
+		  fakeUser2.setId(1);
+		  fakeUser2.setName("Steva");
+		  
+		  User fakeUser3 = new User();
+		  fakeUser3.setId(2);
+		  fakeUser3.setName("Nikoleta");
+
+		  fakeData.add(fakeUser1);
+		  fakeData.add(fakeUser2);
+		  fakeData.add(fakeUser3);
+		  
+		  return fakeData;
+	  }
+}
